bde30c1881c447bd6f90286bf4fc83df3279f32f
micaromano <micaromanow7@gmail.com>
2025-01-24 12:09:44 -0300

    feat: fix some handlers functions that had just async function insted of export default async function

--- tanbronceados/pages/api/register.js
+++ tanbronceados/pages/api/register.js
@@ -8,7 +8,7 @@ const jwtSecret = globals.jwt_secret;
 const secretKey = globals.secret_key;
 const emailUser = globals.email_user;
 
-async function handler(req, res) {
+export default async function handler(req, res) {
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
